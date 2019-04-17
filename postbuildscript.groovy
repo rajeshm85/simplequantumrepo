e29d47661bb4197d9a2486b8773bb5da6a73f408
rajeshm85 <31840014+rajeshm85@users.noreply.github.com>
2019-04-16 21:25:13 -0400

    Update postbuildscript.groovy

--- postbuildscript.groovy
+++ postbuildscript.groovy
@@ -1,4 +1,2 @@
-jobName = manager.build.getProject().getName()
-buildNumber = manager.build.getNumber()
 summary = manager.createSummary("graph.gif")
-summary.appendText("<a href=\"https://ps.reporting.perfectomobile.com/library?jobName[0]=${jobName}&jobNumber[0]=${buildNumber}\">Perfecto Test Report</a>", false)
+summary.appendText("<a href=\"https://ps.reporting.perfectomobile.com/library?jobName[0]=$JOB_NAME&jobNumber[0]=$BUILD_NUMBER\">Perfecto Test Report</a>", false)
